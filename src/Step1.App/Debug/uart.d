42ac9d9da099ae258fddc562e0b4d9a398140125
z0devel <74740032+z0devel@users.noreply.github.com>
2022-02-18 22:57:24 +0100

    Add files via upload

---
+++ src/Step1.App/Debug/uart.d
@@ -0,0 +1,69 @@
+# FIXED
+
+uart.obj: ../uart.c
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/math.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_defs.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_limits.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/string.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/xlocale/_string.h
+uart.obj: ../uart.h
+uart.obj: /home/avives/ti/ccs1110/ccs/ccs_base/msp430/include/msp430.h
+uart.obj: /home/avives/ti/ccs1110/ccs/ccs_base/msp430/include/msp430g2553.h
+uart.obj: /home/avives/ti/ccs1110/ccs/ccs_base/msp430/include/in430.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h
+uart.obj: /home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h
+
+../uart.c:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/math.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_ti_config.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/linkage.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_defs.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_types.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/cdefs.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_types.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_limits.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/string.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/xlocale/_string.h:
+
+../uart.h:
+
+/home/avives/ti/ccs1110/ccs/ccs_base/msp430/include/msp430.h:
+
+/home/avives/ti/ccs1110/ccs/ccs_base/msp430/include/msp430g2553.h:
+
+/home/avives/ti/ccs1110/ccs/ccs_base/msp430/include/in430.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/intrinsics_legacy_undefs.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/stdint.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/_stdint40.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/stdint.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/machine/_stdint.h:
+
+/home/avives/ti/ccs1110/ccs/tools/compiler/ti-cgt-msp430_21.6.0.LTS/include/sys/_stdint.h:
+
---
+++ src/Step2.App/main.c
@@ -0,0 +1,135 @@
+/** MIT License
+ * Copyright (c) 2021, Z0 Devel
+ * All rights reserved.
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ **/
+
+#include <msp430.h>
+#include <uart.h>
+
+#define LED_GREEN_PORT          P1OUT
+#define LED_GREEN_BIT           BIT0
+
+#define COUNTER_VAL             60000
+#define BSL_Address             0xC000
+
+
+volatile static uint8_t rx_char;
+void uart0_rx_byte(uint8_t ch)
+{
+    rx_char = ch;
+}
+
+int main(void)
+{
+    /* Stop watchdog timer */
+    WDTCTL = WDTPW | WDTHOLD;
+
+    /* Define P1.0 as an output */
+    P1DIR |= 0x01;
+
+    /* Enable CCR0 interrupt */
+    CCTL0 = CCIE;
+
+    /* Define CCR0 value */
+    CCR0 = COUNTER_VAL;
+
+    /* Configure SMCLK, mode and divider for CCR0 */
+    TACTL = TASSEL_2 + MC_2 + ID_3;
+
+    /* Initialize UART */
+    uart0_init(9600, uart0_rx_byte);
+
+    /* Enable interrupts */
+    __enable_interrupt();
+
+    while(1)
+    {
+        /*
+         * send console command
+         */
+        uart0_tx_string("Command menu \n");
+        uart0_tx_string("  1) Led ON \n");
+        uart0_tx_string("  2) Led OFF \n");
+        uart0_tx_string("  3) Jump to 0xC000 \n");
+        uart0_tx_string("CMD>");
+
+        __bis_SR_register(LPM0_bits + GIE);    // Enter LPM0, interrupts enabled
+
+        /*
+         *  setting LED on command
+         */
+        if (rx_char == '1')
+        {
+            __disable_interrupt();
+            CCTL0 = CCIE;
+            __enable_interrupt();
+
+            uart0_tx_string("Turning LED ON - OK!\n");
+        }
+        /*
+         *  setting LED off command
+         */
+        else if (rx_char == '2')
+        {
+            __disable_interrupt();
+            CCTL0 = 0;
+            /* make sure the led goes off when disabling it */
+            LED_GREEN_PORT &= ~LED_GREEN_BIT;
+            __enable_interrupt();
+
+
+            uart0_tx_string("Turning LED OFF - OK!\n");
+        }
+        /*
+         *  Jump
+         */
+        else if (rx_char == '3')
+        {
+            uart0_tx_string("Jumping to BSL\n");
+            __disable_interrupt();
+            ((void (*)())BSL_Address)();
+        }
+        /**
+         * Invalid
+         */
+        else
+        {
+            uart0_tx_string("Invalid option\n");
+        }
+    }
+}
+
+// Timer A0 interrupt service routine
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A (void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(TIMER0_A0_VECTOR))) Timer_A (void)
+#else
+#error Compiler not supported!
+#endif
+{
+    LED_GREEN_PORT ^= LED_GREEN_BIT;
+    CCR0 += COUNTER_VAL;
+}
